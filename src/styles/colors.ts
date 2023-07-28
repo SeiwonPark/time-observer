64473f2bce05ef15a8d5042862f47cb1fa39d429
yaehee <yaehee@kakao.com>
2023-07-29 00:42:24 +0900

    box-shadow변경

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,5 +2,5 @@ export const COLORS = {
   primary: '#006400',
   secondary: '#cacaca',
   box_shadow01: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
-  box_shadow02: 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px',
+  box_shadow02: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
 }
