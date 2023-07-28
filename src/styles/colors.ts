1c6372b0fe63a80c69e7c354065e0faaa5cd0379
yaehee <yaehee@kakao.com>
2023-07-29 00:29:21 +0900

    edit box shadow

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,6 @@
 export const COLORS = {
   primary: '#006400',
   secondary: '#cacaca',
-  box_shadow01: 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px',
-  box_shadow02: 'rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset',
+  box_shadow01: 'rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
+  box_shadow02: 'rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px',
 }
