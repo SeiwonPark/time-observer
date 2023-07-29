5ef0cd9ab924b2817980fc5c525a0df76a27cd26
SeiwonPark <psw7347@gmail.com>
2023-07-29 21:54:29 +0900

    feat: add tab pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { COLORS } from './styles/colors'
+import React from 'react'
 
-const Margin4 = styled.div`
-  margin: 4px;
-`
-const Container = styled.div`
-  width: 500px;
-  height: 500px;
-`
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
 
-const CardList = styled.ul`
-  list-style-type: none;
-  padding-left: 0;
-  display: grid;
-  grid-template-columns: repeat(3, 33%);
-  grid-template-rows: repeat(3, auto);
-`
-const Card = styled.li`
-  margin: 8px 4px;
-  padding: 1em;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  border-radius: 8px;
-
-  box-shadow: ${COLORS.box_shadow01};
-  &:hover {
-    box-shadow: ${COLORS.box_shadow02};
-  }
-`
-const Pad2 = styled.div`
-  padding: 2px;
-`
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import NotificationsPage from './pages/NotificationsPage'
+import SettingsPage from './pages/SettingsPage'
+import WeeklyDetailsPage from './pages/WeeklyDetailsPage'
 
 export default function App() {
-  const [currentURL, setCurrentURL] = useState<string>()
-  const [storageData, setStorageData] = useState<StorageData>({})
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      setCurrentURL(tabs[0].url)
-    })
-  }, [])
-
-  useEffect(() => {
-    chrome.storage.local.get(null, (result) => {
-      setStorageData(result)
-    })
-
-    const onChange = (_: { [key: string]: chrome.storage.StorageChange }) => {
-      chrome.storage.local.get(null, (result) => {
-        setStorageData(result)
-      })
-    }
-    chrome.storage.onChanged.addListener(onChange)
-
-    return () => {
-      chrome.storage.onChanged.removeListener(onChange)
-    }
-  }, [])
-
   return (
-    <Container>
-      <Margin4>
-        <div>Current URL: {currentURL}</div>
-        <div>Time spent:</div>
-        <CardList>
-          {Object.entries(storageData).map(([key, value]) => (
-            <Card key={key}>
-              <img src={value.favicon} alt="favicon" width="30" />
-              <Pad2>{key}</Pad2>
-              <Pad2>{value.timeSpent} seconds</Pad2>
-            </Card>
-          ))}
-        </CardList>
-      </Margin4>
-    </Container>
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<WeeklyDetailsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/notification" element={<NotificationsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </MemoryRouter>
   )
 }
---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Layout } from '../components/Layout'
+
+export default function SettingsPage() {
+  return <Layout></Layout>
+}
