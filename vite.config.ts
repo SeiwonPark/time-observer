c8ba95c56dd45b25b704ae294cd794d86152df62
SeiwonPark <psw7347@gmail.com>
2023-07-29 17:27:31 +0900

    add swc plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   root: __dirname,
   publicDir: 'public',
   resolve: {},
