270444efba3251463827d03bce20e76272c86c5a
SeiwonPark <psw7347@gmail.com>
2023-07-30 19:45:50 +0900

    feat: add util function for formating date

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,21 @@
 export const sortByTimeSpent = (a: [string, StorageItem], b: [string, StorageItem]) => b[1].timeSpent - a[1].timeSpent
 
 /**
+ * Returns formatted date
+ * @param {Date} date - Date
+ * @returns {string} Formatted date string
+ * @example
+ * // returns "2023-07-30"
+ * formatDate()
+ */
+export const formatDate = (date: Date = new Date()): string => {
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString().split('T')[0]
+}
+
+/**
  * Returns formatted times
  * @param {number} time - Time in seconds
- *
+ * @returns {string} Formatted time string
  * @example
  * // Returns "01h 01m 02s"
  * formatTime(3662)
