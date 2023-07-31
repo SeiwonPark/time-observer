c32c3bb28f64bfcc40f57e6191b50c273c3e7ce0
SeiwonPark <psw7347@gmail.com>
2023-07-31 18:42:12 +0900

    chore(style): formated

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react-swc'
+
 import path from 'path'
+
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
