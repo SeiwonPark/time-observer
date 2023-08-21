15dd2e841f761d03ac2bfd5abdbee6b65e49bddb
SeiwonPark <psw7347@gmail.com>
2023-08-21 22:53:29 +0900

    feat: setup for svg image

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 /// <reference types="vitest" />
-
-import path from 'path'
+/// <reference types="vite-plugin-svgr/client" />
 
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), cssInjectedByJsPlugin()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    cssInjectedByJsPlugin(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
   root: __dirname,
   publicDir: 'public',
   resolve: {},
