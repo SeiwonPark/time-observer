8fbc065268c6ce934ec44d8d3b06c4d2f719ac37
yaehee <yaehee@kakao.com>
2023-08-21 23:27:05 +0900

    apply global font

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -5,9 +5,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  
+  @import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+
   body {
     margin: 0;
     padding: 0;
+    font-family: 'Rubik', sans-serif;
   }
 
   /* Scrollbar Style */
