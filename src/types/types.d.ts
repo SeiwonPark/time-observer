70182571d388dcf8f1bd864fca52c27c584b3ecb
SeiwonPark <psw7347@gmail.com>
2023-08-22 11:36:56 +0900

    feat: add types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,9 +1,28 @@
 /**
+ * Represents the SVG component property.
+ */
+type SVGProps = React.FC<React.SVGProps<SVGElement>>
+
+/**
+ * Represents the different units that can be displayed within a widget.
+ */
+type WidgetUnit = 'percent' | 'time'
+
+/**
  * Daily storage data for each website
  */
 interface DailyStorageItem {
+  /**
+   * `YYYY-MM-DD` format date string.
+   */
   date?: string
+  /**
+   * The time spent on the item.
+   */
   timeSpent: number
+  /**
+   * Favicon URL of the item.
+   */
   favicon: string
 }
 
