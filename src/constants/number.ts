23fa1ed9a0592588eb174c99f8d6b8bc13c71251
yaehee <yaehee@kakao.com>
2023-08-31 21:09:36 +0900

    chore: add description about UPDATE_CALENDAR_INTERVAL

--- src/constants/number.ts
+++ src/constants/number.ts
@@ -1,1 +1,4 @@
-export const UPDATE_CALENDAR_INTERVAL = 60 // The interval, in seconds, at which the calendar should be updated.
+/**
+The interval, in seconds, at which the calendar should be updated. 
+*/
+export const UPDATE_CALENDAR_INTERVAL = 60
